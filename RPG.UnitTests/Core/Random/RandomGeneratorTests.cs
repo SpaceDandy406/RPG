296abf7674480a8fbb4896e62f9ceee6cd311f94
Red <kandakov.sasha@gmail.com>
2017-10-01 21:55:17 +0500

    Feature/issue 14 (#20)
    * Добавлен статический класс RandomGenerator, способный генерировать псевдослучайные числа с равномерным и нормальным распределением, а так же различными параметрами. Класс покрыт Unit тестами.

---
+++ RPG.UnitTests/Core/Random/RandomGeneratorTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using RPG.Common;
+
+namespace RPG.UnitTests.Core.Random
+{
+    [TestFixture]
+    class RandomGeneratorTests
+    {
+        [Test]
+        public void NextIntEven_genX1X2_X1notEqualX2()
+        {
+            // Arrange
+            int x1;
+            int x2;
+
+            // Act
+            x1 = RandomGenerator.NextIntEven();
+            x2 = RandomGenerator.NextIntEven();
+
+            // Assert
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [Test]
+        public void NextDoubleEven_genX1X2_X1notEqualX2()
+        {
+            // Arrange
+            double x1;
+            double x2;
+
+            // Act
+            x1 = RandomGenerator.NextDoubleEven();
+            x2 = RandomGenerator.NextDoubleEven();
+
+            // Assert
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [Test]
+        public void NextIntEven_gen1000valueFrom5to8_allValueIncludedFrom5to8()
+        {
+            // Arrange
+            int x1 = 5;
+            int x2 = 8;
+            var values = new List<int>();
+
+            // Act
+            for (int i = 0; i < 1000; i++)
+            {
+                values.Add(RandomGenerator.NextIntEven(x1, x2));
+            }
+
+            // Assert
+            var isInInterval = values.Any(x => x < 5 || x >= 8);
+            values.ForEach(x => Console.WriteLine(x));
+            Assert.IsFalse(isInInterval);
+        }
+
+        [Test]
+        public void NextDoubleNormal_genX1X2_X1notEqualX2()
+        {
+            // Arrange
+            double x1;
+            double x2;
+
+            // Act
+            x1 = RandomGenerator.NextDoubleNormal();
+            x2 = RandomGenerator.NextDoubleNormal();
+
+            // Assert
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [Test]
+        public void NextDoubleNormal_genX1X2withParametrs_X1notEqualX2()
+        {
+            // Arrange
+            double x1;
+            double x2;
+
+            // Act
+            x1 = RandomGenerator.NextDoubleNormal(10.5, 1.3);
+            x2 = RandomGenerator.NextDoubleNormal(10.5, 1.3);
+
+            // Assert
+            Assert.AreNotEqual(x1, x2);
+        }
+    }
+}
---
+++ RPG/Common/RandomGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RPG.Common
+{
+    public static class RandomGenerator
+    {
+        // Теория получения псевдослучайного числа с нормальным распределением 
+        // взята из методических материалов выданных Максимовым П.В.
+
+        private readonly static Random random = new Random();
+
+        public static int NextIntEven()
+        {
+            return random.Next();
+        }
+
+        public static int NextIntEven(int a, int b)
+        {
+            return random.Next(a, b);
+        }
+
+        public static double NextDoubleEven()
+        {
+            return random.NextDouble();
+        }
+
+        public static double NextDoubleNormal()
+        {
+            double r = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                r += random.NextDouble();
+            }
+            r -= 6;
+            return r;
+        }
+
+        public static double NextDoubleNormal(double m, double sigma)
+        {
+            return m * sigma * NextDoubleNormal();
+        }
+    }
+}
