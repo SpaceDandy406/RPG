b5d4c222026812ef8af7e123dbd8ec20b8ed4ddc
Space Dandy <rocky.j59@yandex.ru>
2017-10-01 12:34:12 +0500

    Add nunit tests (#17)
    * создал тестовую сборку
    
    * добавил из нугета nunit и создал первые папки
    
    * добавив InternalVisibilityTo для тестов, в тестах добавил ссылку на MonoGame

---
+++ RPG.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RPG.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("RPG.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b365387-c669-406f-97d1-b5552afb314d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
